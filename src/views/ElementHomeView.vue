2ceb0a1df52bd677c2d875ef45ecd9fa002b4355
BWS <tanguo@hotmail.com>
2023-08-02 11:38:30 +0800

    Update ElementHomeView.vue

--- src/views/ElementHomeView.vue
+++ src/views/ElementHomeView.vue
@@ -9,17 +9,21 @@
                 <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
             </el-tabs>
         </div>
-        <div>
+        <div class="topsection">
             <el-card :body-style="{ padding: '0px' }">
-            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-            <div style="padding: 14px;">
-                <span>好吃的汉堡</span>
-                <div class="bottom clearfix">
-                <time class="time">{{ currentDate }}</time>
-                <el-button type="text" class="button">操作按钮</el-button>
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+                <div style="padding: 14px;">
+                    <span>好吃的汉堡</span>
+                    <div class="bottom clearfix">
+                    <time class="time">{{ currentDate }}</time>
+                    <el-button type="text" class="button">操作按钮</el-button>
+                    </div>
                 </div>
-            </div>
             </el-card>
+        </div>
+        <div class="middlesection">
+        </div>
+        <div class="bottomsection">
         </div>
     </div>
 </template>
