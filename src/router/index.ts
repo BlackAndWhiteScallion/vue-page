d3d494c2d3b8607dd5089978556e897def3e3a31
BWS <tanguo@hotmail.com>
2023-08-08 15:08:12 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ const router = new VueRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: {SideNav, BreadcrumbsPP},
+      component: SideNav,
       children:[
         {
           path: 'home',
